77f2a98d2d32b765dff033b4d667a7a2053803ed
Claire Blackshaw <dumper01@flammablepenguins.com>
2012-12-30 01:26:38 +0000

    Working Data Transfer

---
+++ main.py
@@ -0,0 +1,297 @@
+#!/usr/bin/env python
+#
+# Copyright 2007 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+import datetime
+import urllib
+import webapp2
+import random
+import json
+import os
+import jinja2
+import logging
+
+from google.appengine.ext import db
+from google.appengine.api import urlfetch
+from google.appengine.api import memcache
+from google.appengine.api import users
+
+jinja_enviroment = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
+
+#---------------------------------------------------------------------
+#   Data Model Notes
+#
+#   BlockArtist < Block < BlockVer
+#---------------------------------------------------------------------
+class BlockArtist(db.Model):
+	user     = db.UserProperty(auto_current_user=True)
+	joined   = db.DateTimeProperty(auto_now_add=True)
+	nickname = db.StringProperty(required=True)
+	artistID = db.IntegerProperty(required=True)						# Artist Number
+	blockMax = db.IntegerProperty(default=256)
+	#xyz - facing
+	x = db.IntegerProperty()
+	y = db.IntegerProperty()
+	z = db.IntegerProperty()
+	f = db.IntegerProperty()
+
+class Block(db.Model):
+	blockID  = db.IntegerProperty(required=True)						# Block Number
+
+# 0 Core   - 3bit - NO DATA - Empty, Shading White 100%, 80%, 60%, 40%, 20%, 0%, Self Ref
+# 1 Colour - 3bit - 8x 24Bit Web RGB - +1 bit if not first colour pal
+# 2 Alpha  - 2bit - NO DATA - 100%, 75%, 50%, 25%
+# 3 Link   - 1bit - NO DATA - Links are childed 
+# 4 Smooth - 1bit - NO DATA -
+# 5 Shape  - 3bit - Shape Date *shrug unkown*
+class BlockVer(db.Model):
+	date      = db.DateTimeProperty(auto_now_add=True)
+	blockData = db.BlobProperty(required=True)						# Save Data
+	palData   = db.ByteStringProperty()                             # Pallette Data
+	linkData  = db.ListProperty(db.Key)								# Links to BLOCK not Block Ver
+
+#---------------------------------------------------------------------
+#	Helpers
+#---------------------------------------------------------------------
+def getArtistByID(artID):
+	if artID == None or len(artID) < 1:
+		usr = users.get_current_user()
+		artist = BlockArtist.gql('WHERE user = :u', u = usr).get()
+		return artist
+
+	artist = BlockArtist.get_by_key_name('aid' + artID)
+	return artist
+
+def getBlockByID(artID, blockID):
+	if blockID == None or len(blockID) < 1:
+		blockID = 0
+
+	block = Block.get(db.Key.from_path('BlockArtist', 'aid' + str(artID), 'Block', 'bid' + str(blockID)))
+	return block
+
+
+#---------------------------------------------------------------------
+#   Handler Notes
+#---------------------------------------------------------------------
+class ArtistHandler(webapp2.RequestHandler):
+	def get(self):
+		self.response.headers['content-type'] = 'application/json; charset=utf-8'
+
+		artist = getArtistByID(self.request.get('aid'))
+
+		if artist == None:			
+			self.response.out.write(json.dumps({"aid": -1, "nickname" : '' }));
+			return
+
+		self.response.out.write(json.dumps({"aid": artist.artistID, "nickname" : artist.nickname }));
+
+
+	def post(self):
+		self.response.headers['content-type'] = 'application/json; charset=utf-8'
+		usr = users.get_current_user()
+
+		artist = getArtistByID(None)
+
+		if artist == None:
+			newNick = self.request.get('newNickname')
+			if newNick == None or len(newNick) < 3:
+				self.response.out.write('No Nickname')
+				self.error(400)
+				return
+
+			myArtID = self.bumpCounter()
+
+			if myArtID > 0:
+				artist = BlockArtist(key_name='aid' + str(myArtID), nickname = newNick, artistID = myArtID)
+				artist.put()
+			else:
+				self.response.out.write('OUT OF RANGE ')
+
+			if artist == None:			
+				self.error(500)
+				return
+			#
+
+		self.response.out.write(json.dumps({"aid": artist.artistID, "nickname" : artist.nickname }));
+
+	def bumpCounter(self):
+		memCl = memcache.Client()
+		counter = memCl.gets('aid')
+		if counter == None:
+			aid = BlockArtist.all(keys_only=True).count()
+			memCl.set('aid', aid)
+			counter = memCl.gets('aid')
+
+		x = 0
+		while x < 10:
+			counter = memCl.get('aid')
+			if memCl.cas('aid', counter+1):
+				return counter+1
+			x += 1
+
+		return -1
+
+class BlockHandler(webapp2.RequestHandler):
+	def get(self):
+		self.response.headers['content-type'] = 'application/json; charset=utf-8'
+
+		# Check if Art ID
+		artist = getArtistByID(self.request.get('aid'))
+
+		if artist == None:
+			self.response.out.write(json.dumps({ "aid": -1 }));
+			return
+
+		# Check Block ID
+		block = getBlockByID(artist.artistID, self.request.get('blockID'))
+
+		if block == None:
+			self.response.out.write(json.dumps({"aid": artist.artistID, "bid" : -1 }));
+			return
+
+		# Else Get Home Block
+		self.response.out.write(json.dumps({"aid": artist.artistID, "bid" : block.blockID }));
+
+	def post(self):
+		# Block Data
+		bData = self.request.get('blockData')
+		pData = self.request.get('palData')
+		lData = self.request.get('linkData')
+
+		# Validate Data
+		if bData == None or pData == None or lData == None or len(bData) < 1 or len(pData) < 1 == None or len(lData) < 1 == None:
+			# You need to pass this data
+			self.error(400)
+
+		self.response.out.write(json.dumps({'blockData':bData, 'palData':pData, 'linkData':lData}));
+		return
+
+		# Check Block ID
+		artist = getArtistByID(None)
+		bid = self.request.get('blockID')
+		block = getBlockByID(artist.artistID, bid)
+
+
+		# Create New Block
+		if block == None:
+			blockList = Block.gql('WHERE ancestor IS :a', a = artist)
+			bid = blockList.count()
+
+			if bid >= artist.blockMax:
+				self.error(500) # Exceeded Limit
+				return
+
+			block = Block(key_name='bid' + str(bid),parent=artist, blockID=bid)
+			block.put()
+			#
+
+		# Make New Version
+		newBVersion = BlockVer(parent=block, blockData=bData, palData=pData, linkData=lData)
+		newBVersion.put()
+
+		self.response.out.write(json.dumps({"aid": artist.artistID, "bid" : block.blockID }));
+		pass
+
+class MainHandler(webapp2.RequestHandler):
+	def get(self):
+		usr = users.get_current_user()
+
+		artist = getArtistByID(None)
+
+		if artist == None:
+			self.response.out.write("""<div class="artist form"><h3>Create Artist Profile</h3>
+				<form action="/artist" method="post">
+				<input type="text" name="newNickname" />
+				<input type="submit" value="Submit" />
+				</form></div>
+				""");
+			return
+		else:
+			self.response.out.write("""
+				Hello %s ! Your #%d
+				""" % (artist.nickname, artist.artistID));
+		#
+
+		blockList = Block.gql('WHERE ancestor IS :a', a = artist)
+
+		self.response.out.write(blockList.count())
+		self.response.out.write('<div class="blockList"><ul>')
+		for block in blockList:
+			self.response.out.write('<li>#%d : ver[#%d]</li>' % (block.blockID, 0))
+
+		self.response.out.write('</ul></div>')
+
+		self.response.out.write("""<div class="block form"><h3>Create Block</h3>
+				<form action="/block" method="post">
+				<input name="colour1" type="color" />
+				<input type="submit" value="New Block" />
+				</form></div>
+				""");
+
+class BlockEditHandler(webapp2.RequestHandler):
+	def get(self):
+		# SETUP
+		artist = getArtistByID(None)
+
+		if artist == None:
+			self.error(500)
+			return
+
+		aid = artist.artistID
+		block = getBlockByID(aid, None)
+
+		if block == None:
+			bid = 0
+		else:
+			bid = block.blockID
+
+		# Get Others
+		blockList = Block.gql('WHERE ancestor IS :a', a = artist)
+
+		# Get Request Data
+		bData = self.request.get('blockData')
+		pData = self.request.get('palData')
+		lData = self.request.get('linkData')
+
+		template_values = {
+			'artistID' : aid,
+			'blockID'  : bid,
+			'blockList': blockList,
+			'blockData': bData, 
+			'palData'  : pData, 
+			'linkData' : lData
+		}
+
+		template = jinja_enviroment.get_template('newBlockVer.html')
+		self.response.out.write(template.render(template_values))
+
+	def post(self):
+		#self.response.headers['Content-Type'] = 'application/octet-stream'
+		#self.response.headers['Content-Encoding'] = 'arraybuffer'
+
+		self.response.headers['Content-Type'] = 'application/boobs; base64'
+		
+		self.response.headers['szBlock']      = str(self.request.get('szBlock'))
+		self.response.headers['szPal']        = str(self.request.get('szPal'))
+		self.response.headers['szLink']       = str(self.request.get('szLink'))
+
+		self.response.body = self.request.body
+
+app = webapp2.WSGIApplication([
+								('/', MainHandler), 
+								('/artist', ArtistHandler), 
+								('/block', BlockHandler),
+								('/blockEditor', BlockEditHandler)],
+								debug=True)
